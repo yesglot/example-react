f1dd7a16a8ece2bf924d782b921df4bd8043d889
Efe Öge <efeoge@gmail.com>
2026-05-10 21:25:09 +0200

    Have languages.js

--- i18next.config.js
+++ i18next.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'i18next-cli'
+import { LANGUAGE_CODES } from './src/languages.js'
 
 /** @type {import('i18next-cli').I18nextToolkitConfig} */
 export default defineConfig({
-  locales: [
-    "en",
-    "de",
-    "es",
-    "fr",
-    "it",
-    "ja",
-    "nl",
-    "pt",
-    "tr"
-  ],
+  locales: LANGUAGE_CODES,
   extract: {
     input: "src/**/*.{js,jsx,ts,tsx}",
     output: "public/locales/{{language}}/{{namespace}}.json"
--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import { LANGUAGE_CODES } from "./languages";
 
 i18n
   .use(HttpBackend)
@@ -9,40 +10,7 @@ i18n
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
-    supportedLngs: [
-      "en",
-      "es",
-      "fr",
-      "de",
-      "pt",
-      "zh-CN",
-      "ja",
-      "ko",
-      "ar",
-      "hi",
-      "it",
-      "nl",
-      "tr",
-      "pl",
-      "ru",
-      "id",
-      "vi",
-      "th",
-      "uk",
-      "cs",
-      "sv",
-      "ro",
-      "el",
-      "da",
-      "fi",
-      "no",
-      "he",
-      "hu",
-      "ms",
-      "fa",
-      "bn",
-      "ta",
-    ],
+    supportedLngs: LANGUAGE_CODES,
     backend: {
       loadPath: "/locales/{{lng}}/translation.json",
     },
